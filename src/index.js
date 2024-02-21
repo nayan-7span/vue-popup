8a887025441574458b194b4b794a42701854649f
nayan-7span <nayan.padashala@gmail.com>
2024-02-21 16:49:28 +0530

    wip

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const defaultComponents = components?.default;
 const vueComponents = {
   install(Vue) {
     Object.keys(defaultComponents).forEach((name) => {
-      Vue.components(name, defaultComponents[name]);
+      console.log("the key is", name);
+      Vue.component(name, defaultComponents[name]);
     });
   },
 };
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   build: {
     lib: {
-      entry: resolve(__dirname, "src/index.js"),
+      entry: resolve(__dirname, "./src/index.js"),
       formats: ["es", "cjs"],
-      fileName: (format) => `vue-components.${format}`,
+      fileName: (format) => `vue-components.${format}.js`,
     },
     rollupOptions: {
       external: ["vue"],
